674de1de09a9dded19e29c4c405216db4becd6e6
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-05-27 13:49:04 +0600

    variable-declaration-and-print

---
+++ script.js
@@ -0,0 +1,13 @@
+function display(){
+    let a = 5;
+    let b = "hi";
+    let c = 3.5;
+    let d = "Hello";
+    const e = "61_A1";
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+    console.log('Button Clicked!')
+}
